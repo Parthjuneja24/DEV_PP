a023488decc4721586efda219816bc9975ac578c
Parth <pathjuneja24@gmail.com>
2022-11-12 12:18:49 +0530

    second

---
+++ Lec0/pp.js
@@ -0,0 +1,4 @@
+/* first step m init kiya           -----------init s github aab apni file ko dekhne lag gaya
+/* first maine stage kiya change -----stilll no change
+/* commit kiya ------------ github still no change hua 
+/* push kiya               -------github m change hua
